d257544b5d5983a451e5960de70a1e608d6b6236
Fábio <47451463+FabioSmuu@users.noreply.github.com>
2024-09-07 10:57:53 -0300

    Update BasicLogicgates.js

--- BasicLogicgates.js
+++ BasicLogicgates.js
@@ -3,3 +3,23 @@ const AND = (a, b) => a & b
 const OR = (a, b) => a | b
 const NOT = (a) => ~a & 1
 const XOR = (a, b) => a ^ b
+
+class ALU {
+  constructor() {}
+
+  add(a, b) {
+    return a + b // Simulação de soma
+  }
+
+  and(a, b) {
+    return AND(a, b)
+  }
+
+  or(a, b) {
+    return OR(a, b)
+  }
+
+  xor(a, b) {
+    return XOR(a, b)
+  }
+}
