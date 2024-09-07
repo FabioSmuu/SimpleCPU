555bdc6635be0230f1cffbf0572bb464fafa0ee2
Fábio <47451463+FabioSmuu@users.noreply.github.com>
2024-09-07 11:05:46 -0300

    Update RAM.js

--- RAM.js
+++ RAM.js
@@ -11,3 +11,11 @@ class RAM {
     this.memory[address] = value
   }
 }
+
+// Exemplo
+// Definir endereços importantes na memória
+const snakeHeadXAddr = 0 // Endereço da posição X da cabeça da cobra
+const snakeHeadYAddr = 1 // Endereço da posição Y da cabeça da cobra
+const foodXAddr = 2      // Endereço da posição X da comida
+const foodYAddr = 3      // Endereço da posição Y da comida
+const directionAddr = 4  // Endereço da direção da cobra
